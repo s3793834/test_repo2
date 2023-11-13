087b8fa26df25f50476960db611ca4fe6e2835ac
Isa Noor <i.n.noor@student.rug.nl>
2023-11-13 12:20:47 +0100

    figure 1 produced

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,12 +1,18 @@
 # first github synced script
 library(renv)
-library(ggplot2)
 # restore versions of the packages used to develop this project
 renv::restore()
-
+#remember to use pull in Git tab to get changes from github
+#load libraries
+library(tidyverse)
 
 x <- c(1,2,3,4,5)
 y <- c(1,4,3,6,9)
 data <- data.frame(x,y)
 data
 
+# |> is new base R pipe, %>% still has additional functionalities
+fig01 <- data |> ggplot(aes(x=x,y=y))+
+  geom_point() +
+  geom_smooth(method='lm')
+ggsave("./figures/fig01.png", plot=fig01, width = 1920, height=1068, units="px")
