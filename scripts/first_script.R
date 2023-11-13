ab31630a22c5636fc2bf6af07f5e4280343f2d9e
Isa Noor <i.n.noor@student.rug.nl>
2023-11-13 12:05:34 +0100

    project initiallized with renv

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,1 +1,12 @@
 # first github synced script
+library(renv)
+library(ggplot2)
+# restore versions of the packages used to develop this project
+renv::restore()
+
+
+x <- c(1,2,3,4,5)
+y <- c(1,4,3,6,9)
+data <- data.frame(x,y)
+data
+
